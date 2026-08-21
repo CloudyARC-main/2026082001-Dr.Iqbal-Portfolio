8f45d8673d87ba9a56f7beafdbfdbd4fbe75ea4a
abdurrahmanc-dev <abdurrahmanc@lambdacrunch.com>
2026-08-21 12:18:03 +0600

    realease version1

--- homeApp/urls.py
+++ homeApp/urls.py
@@ -5,4 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home_func, name="home_func"),
+    path('about-us', views.about_us_func, name="about_us_func"),
+    path('timeline/', views.timeline_func, name='timeline_func'),
+    path('gallery/', views.gallery_func, name='gallery_func'),
+    path('videos/', views.videos_func, name='videos_func'),
+    path('events/', views.events_func, name='events_func'),
+    path('news/', views.news_func, name='news_func'),
+    path('shop/', views.shop_func, name='shop_func'),
+    path('contact/', views.contact_func, name='contact_func'),
 ]
--- homeApp/views.py
+++ homeApp/views.py
@@ -6,3 +6,33 @@ from django.shortcuts import render
 
 def home_func(request):
     return render(request, "home.html")
+
+def about_us_func(request):
+    return render(request, "about_us.html")
+
+def timeline_func(request):
+    return render(request, "timeline.html")
+
+def gallery_func(request):
+    return render(request, "gallery.html")
+
+def videos_func(request):
+    return render(request, "videos.html")
+
+def events_func(request):
+    return render(request, "events.html")
+
+def news_func(request):
+    return render(request, "news.html")
+
+def shop_func(request):
+    return render(request, "shop.html")
+
+def contact_func(request):
+    return render(request, "contact.html")
+
+def shop_func(request):
+    return render(request, "shop.html")
+
+def shop_func(request):
+    return render(request, "shop.html")
